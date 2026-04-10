d678079ec3ff1a5c0c26af64f0587f28de38dac0
Artrax90 <67760877+Artrax90@users.noreply.github.com>
2026-04-10 11:56:00 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ export type Note = {
   content: string;
   folderId?: string;
   isShared?: boolean;
+  isSharedByMe?: boolean;
   ownerUsername?: string;
   permission?: 'read' | 'write' | 'owner';
   isPinned?: boolean;
@@ -29,6 +30,7 @@ export type Folder = {
   name: string;
   parentId?: string;
   isShared?: boolean;
+  isSharedByMe?: boolean;
   ownerUsername?: string;
   permission?: 'read' | 'write' | 'owner';
 };
